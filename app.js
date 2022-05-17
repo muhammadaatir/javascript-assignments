16d0f1f7e4bc0c1e5411db1bf65532017bb84168
muhammadaatir <aatirmemon.8211@gmail.com>
2022-05-18 00:27:13 +0500

    Assignment of Chap 1 Done!

---
+++ app.js
@@ -0,0 +1,20 @@
+// Chapter No 1
+// Question No 1
+alert("Chapter 1 of JavaScript");
+
+// Question No 2
+alert("Error! Please enter a valid password");
+
+// Question No 3
+alert("Welcome to JS Land... \nHappy Coding!");
+
+// Question No 4
+alert("Welcome to JS Land...");
+
+alert("Happy Coding \nPrevent this page from creating additional dialogs.");
+
+// Question No 5
+alert("Hello... I  can run JS through my web browser console");
+
+
+alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬ ");
