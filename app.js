3a5f13e4d3e141e43695ed1fba7783c10c03eab4
muhammadaatir <aatirmemon.8211@gmail.com>
2022-05-18 01:11:24 +0500

    Chapter No 2's Assignment Done!

--- app.js
+++ app.js
@@ -17,4 +17,61 @@ alert("Happy Coding \nPrevent this page from creating additional dialogs.");
 alert("Hello... I  can run JS through my web browser console");
 
 
-alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬ ");
+// alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬ ");
+
+
+// Chapter No 2
+// Question No 1
+var username;
+
+// Question No 2
+var myname = "Muhammad Aatir";
+
+
+// Question No 3
+
+var message = "Hello World";
+
+alert(message);
+
+// Question No 4
+
+var age = "18 years old"
+var study = "I am student of Web & App development at SMIT"
+
+alert(myname);
+alert(age);
+alert(study);
+
+// Question No 5
+
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+
+alert(pizza);
+
+// Question No 6
+
+var email = "muhammadaatir.30@gmail.com"
+
+alert("My Email Address is " + email);
+
+// Question No 7
+
+var book = "A smarter way to learn JavaScript"
+
+alert("I am trying to learn from a book " + book);
+
+// Question No 8
+
+document.write ("Yah! I can write HTML content through JavaScript");
+
+// Question No 9
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design);
+
+
+
+
+
