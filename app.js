1f8ae07fa304195d1b647e3fc53b5d52c06dce26
muhammadaatir <aatirmemon.8211@gmail.com>
2022-05-20 17:16:02 +0500

    Chap 4 & 5 Done !

--- app.js
+++ app.js
@@ -1,100 +1,266 @@
-// Chapter No 1
-// Question No 1
-alert("Chapter 1 of JavaScript");
+// // Chapter No 1
+// // Question No 1
+// alert("Chapter 1 of JavaScript");
 
-// Question No 2
-alert("Error! Please enter a valid password");
+// // Question No 2
+// alert("Error! Please enter a valid password");
 
-// Question No 3
-alert("Welcome to JS Land... \nHappy Coding!");
+// // Question No 3
+// alert("Welcome to JS Land... \nHappy Coding!");
 
-// Question No 4
-alert("Welcome to JS Land...");
+// // Question No 4
+// alert("Welcome to JS Land...");
 
-alert("Happy Coding \nPrevent this page from creating additional dialogs.");
+// alert("Happy Coding \nPrevent this page from creating additional dialogs.");
 
-// Question No 5
-alert("Hello... I  can run JS through my web browser console");
-
-
-// Chapter No 2
-// Question No 1
-var username;
-
-// Question No 2
-var myname = "Muhammad Aatir";
+// // Question No 5
+// alert("Hello... I  can run JS through my web browser console");
 
 
-// Question No 3
+// // Chapter No 2
+// // Question No 1
+// var username;
 
-var message = "Hello World";
+// // Question No 2
+// var myname = "Muhammad Aatir";
 
-alert(message);
 
-// Question No 4
+// // Question No 3
 
-var age = "18 years old"
-var study = "I am student of Web & App development at SMIT"
+// var message = "Hello World";
 
-alert(myname);
-alert(age);
-alert(study);
+// alert(message);
 
-// Question No 5
+// // Question No 4
 
-var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// var age = "18 years old"
+// var study = "I am student of Web & App development at SMIT"
 
-alert(pizza);
+// alert(myname);
+// alert(age);
+// alert(study);
 
-// Question No 6
+// // Question No 5
 
-var email = "muhammadaatir.30@gmail.com"
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
 
-alert("My Email Address is " + email);
+// alert(pizza);
 
-// Question No 7
+// // Question No 6
 
-var book = "A smarter way to learn JavaScript"
+// var email = "muhammadaatir.30@gmail.com"
 
-alert("I am trying to learn from a book " + book);
+// alert("My Email Address is " + email);
 
-// Question No 8
+// // Question No 7
 
-document.write ("Yah! I can write HTML content through JavaScript");
+// var book = "A smarter way to learn JavaScript"
 
-// Question No 9
+// alert("I am trying to learn from a book " + book);
 
-var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// // Question No 8
 
-alert(design);
+// document.write ("Yah! I can write HTML content through JavaScript");
 
-// Chapter No 3
-// Question No 1
+// // Question No 9
 
-var myage =  18; 
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
 
-alert("I am " + myage + " years old.");
+// alert(design);
 
-// Question No 2
+// // Chapter No 3
+// // Question No 1
 
-var visit =  2; 
+// var myage =  18; 
 
-alert("You have visited this site " + visit + " times" );
+// alert("I am " + myage + " years old.");
 
-// Question No 3
+// // Question No 2
 
-var birthdate = 2003;
+// var visit =  2; 
 
-document.write("My birth year is " + birthdate );
-document.write("<br>");
-document.write("  Data type of my declared variable is Number.")
+// alert("You have visited this site " + visit + " times" );
 
-// Question No 4
+// // Question No 3
 
-var name = "John Doe"
-var num1 = 5
-var thing = " T-Shirts(s)"
+// var birthdate = 2003;
+// // Question No 2 
 
-document.write("<br> <br>");
-document.write(name + " ordered " + num1 + thing + " on XYZ Clothing Store")
+// document.write("My birth year is " + birthdate );
+// document.write("<br>");
+// document.write("  Data type of my declared variable is Number.")
+
+// // Question No 4
+
+// var name = "John Doe";
+// var num1 = 5;
+// var thing = " T-Shirts(s)";
+
+// document.write("<br> <br>");
+// document.write(name + " ordered " + num1 + thing + " on XYZ Clothing Store")
+
+//  // Chapter No 4
+//  // Question No 1 
+
+// var intro = "I am learning JS ";
+// var done = "I have completed HTML, CSS & BOOTSTRAP";
+// var  student = "I am learning web & app development at SMIT ";
+
+// // Question No 2 
+
+// //legal variable names
+// var myvar;
+// var javascript;
+// var number1;
+// var _it;
+// var $mine;
+
+// // illegal variable names
+// var var;
+// var 123;
+// var java script;
+// var -js;
+// var 1st;
+
+// // Question No 3 
+
+// document.write ( "<h1>" + "Rules for naming JS variables" + "</h1>")
+// document.write ("Variable names can only contain letters, numbers, $ and _ .<br/>")
+// document.write ("Variable must begin with a letter, _ or $ .<br/>")
+// document.write ("Variable names are case  sensitive.<br/>")
+// document.write ("Variable names should not be JS keywords.")
+
+// // Chapter No 5
+// // Question No 1
+// var sum = 2+7;
+// document.write ("Sum of 2 and 7 is " + sum)
+
+// // Question No 2
+// var product = 9*6;
+// var difference = 5-1;
+// var quotient = 8/4;
+// var modulus = 17%5;
+
+// document.write ("<br/>Difference of 5 and 1 is " + difference)
+// document.write ("<br/>Product of 9 and 6 is " + product)
+// document.write ("<br/>Quotient of 8 and 4 is " + quotient)
+// document.write ("<br/>Modulus of 17 and 5 is " + modulus)
+
+// // Question No 3
+// var theNumber = 7;
+
+// document.write("<br/><br/>Value after declaration:  " + "<b>" + theNumber  + "</b>")
+// document.write("<br/>Initial Value:  " + "<b>" + theNumber  + "</b>")
+// document.write("<br/>Value after increament:  " + "<b>" + ++theNumber + "</b>" )
+// theNumber = theNumber + 7
+// document.write("<br/>Value after adding 7:  " + "<b>" + theNumber  + "</b>")
+// document.write("<br/>Value after decreament:  " + "<b>" + --theNumber + "</b>" )
+// theNumber = theNumber % 2
+// document.write("<br/>The Remainder after dividing by 2:  " + "<b>" + theNumber  + "</b>")
+
+// // Question No 4 
+// document.write("<h4>" + "If cost of one movie ticket is 600 PKR" + "</h4>")
+// document.write("<h4>" + "Then Cost of 5 tickets will be " + 600*5 + "</h4>" )
+
+// // Question No 5
+// var count = 1
+// var table = 12
+// document.write("<h4>" + "Table of 12" + "</h4>")
+// document.write("<h4>" + table + " x " + count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+// document.write("<h4>" + table + " x " + ++count + " = " + table*count + "</h4>")
+
+// // Question No 6
+// var celcius = 26
+// var fahrenheit = 72
+
+// document.write("<h4>" + celcius + "°C is " + ((celcius*9/5)+32) + "°F" + "</h4>")
+// document.write("<h4>" + fahrenheit + "°F is " + ((fahrenheit-32)*5/9) + "°C" + "</h4>")
+
+// // Question No 7
+
+// document.write("<h1>" + "Shopping Cart" + "</h1>")
+
+// var item1p = 680
+// var item1q = 2
+// var item2p = 820
+// var item2q = 3
+// var shipping = 250
+
+// document.write("<h4>" + "Price is item 1 is " + item1p + "</h4>")
+// document.write("<h4>" + "Quantity is item 1 is " + item1q + "</h4>")
+// document.write("<h4>" + "Price is item 2 is " + item2p + "</h4>")
+// document.write("<h4>" + "Quantity is item 1 is " + item2q + "</h4>")
+// document.write("<h4>" + "Shipping Charges " + shipping + "</h4>")
+
+// document.write("<h4>" + "<br/><br/>Total cost of your order is " + ((item1p*item1q)+(item2p*item2q)+shipping) + "</h4>")
+
+// // Question No 8
+
+// var obtMarks = 923
+// var totMarks = 1100
+
+// document.write ( "<h1>" + "Marks Sheet" + "</h1>")
+// document.write("<h4>" + "Total Marks:  " + totMarks + "</h4>")
+// document.write("<h4>" + "Obtained Marks:  " + obtMarks + "</h4>")
+// document.write("<h4>" + "Percentage:  " + (obtMarks/totMarks*100) + "</h4>")
+
+// // Question No 9
+
+// var dollar = 10
+// var riyal = 25
+
+// document.write ( "<h1>" + "Currency in PKR" + "</h1>")
+// document.write("<h4>" + "If we have " + dollar + " US Dollars & " + riyal + " Saudi Riyals" + "</h4>")
+// document.write("<h4>" + "Total  Amount in PKR  " + ((dollar*201.30)+(riyal*53.67)) + "</h4>")
+
+// // Question No 10
+
+// var thenum = 11
+
+// document.write("<h4>" + (((thenum+5)*10)/2) + "</h4>")
+
+// // Question No 11
+
+// var curYear = 2022;
+// var birthYear = 2003;
+
+// document.write ( "<h1>" + "Age Calculator" + "</h1>")
+// document.write("<h4>" + "Current Year:  " + curYear + "</h4>")
+// document.write("<h4>" + "Birth Year:  " + birthYear + "</h4>")
+// document.write("<h4>" + "Your Age:  " + (curYear-birthYear) + "</h4>")
+
+// // Question No 12
+
+// var radius = 18;
+
+// document.write ( "<h1>" + "The Geometrizer" + "</h1>")
+// document.write("<h4>" + "Radius of a Circle:  " + radius + "</h4>")
+// document.write("<h4>" + "The Circumference of Circle is:  " + (2*3.142*radius) + "</h4>")
+// document.write("<h4>" + "The Area of Circle is:  " + (3.142*(radius*radius)) + "</h4>")
+
+// // Question No 13
+
+// var favSnack = "  burger  ";
+// var currentAge = 18;
+// var maxage = 72;
+// var amount = 2;
+
+// document.write("<h1>" + "The Life Time Supply Calculator" + "</h1>")
+// document.write("<h4>" + "Favourite Snack:  " + favSnack + "</h4>")
+// document.write("<h4>" + "Current Age:  " + currentAge + "</h4>")
+// document.write("<h4>" + "Estimated Maximum Age:  " + maxage + "</h4>")
+// document.write("<h4>" + "Amount Snacks Per Day:  " + amount + "</h4>")
+// document.write("<h4>" + "You will need  " + ((maxage - currentAge) * amount) + favSnack + "to last you until the ripe old age of  " + maxage + "</h4>")
+
+
+
+
 
